adc00a9f0ba4bc9ec8beff1eedc66c374b0272af
ice cold <wargamingnl7@gmail.com>
2017-12-01 17:03:46 +0100

    almost done

--- SpawnsManager.cs
+++ SpawnsManager.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Fougerite;
 using Fougerite.Events;
 using UnityEngine;
 using RustProto;
+using Facepunch.ID;
 
 namespace SpawnsManager
 {
@@ -18,10 +20,7 @@ namespace SpawnsManager
 
         public string green = "[color #82FA58]";
         public string red = "[color #B40404]";
-        public static IniParser ini;
-        public static IniParser Spawns;
-        public static string Path;
-        public static string Combine;
+        public static IniParser Settings;
 
 
         public override void Initialize()
@@ -29,22 +28,18 @@ namespace SpawnsManager
             if (!File.Exists(Path.Combine(ModuleFolder, "Spawns.ini")))
             {
                 File.Create(Path.Combine(ModuleFolder, "Spawns.ini")).Dispose();
-                Spawns = new IniParser(Path.Combine(ModuleFolder, "Spawns.ini"));
-                ini.AddSetting("Spawns", "Spawn1", "6600, 356, -4400");
-                Spawns.Save();
+                Settings = new IniParser(Path.Combine(ModuleFolder, "Spawns.ini"));
+                Settings.AddSetting("Spawns", "Spawn1", "6600, 356, -4400");
+                Settings.Save();
             }
-            ReloadSpawns();
             Fougerite.Hooks.OnCommand += OnCommand;
             Fougerite.Hooks.OnPlayerSpawned += OnPlayerSpawned;
-            Fougerite.Hooks.OnPlayerTeleport += OnPlayerTeleport;
-            rnd = new System.Random();
-            ini = new IniParser(Path.Combine(ModuleFolder, "Spawns.ini"));
+            Settings = new IniParser(Path.Combine(ModuleFolder, "Spawns.ini"));
         }
         public override void DeInitialize()
         {
             Fougerite.Hooks.OnCommand += OnCommand;
             Fougerite.Hooks.OnPlayerSpawned += OnPlayerSpawned;
-            Fougerite.Hooks.OnPlayerTeleport += OnPlayerTeleport;
         }
         public void OnCommand(Fougerite.Player Player, string cmd, string[] args)
         {
@@ -61,37 +56,27 @@ namespace SpawnsManager
                     Player.MessageFrom("SpawnsManager", green + "/spawndel - deletes a spawn.");
                     Player.MessageFrom("SpawnsManager", green + "/spawnsreload - reloads the spawns list.");
                 }
-                if (cmd == "spawnadd")
+            }
+            else if (cmd == "spawnadd")
+            {
+                if (!Player.Admin)
                 {
-                    if (!Player.Admin)
+                    Player.MessageFrom("SpawnsManager", red + "You are not an administrator to use this command.");
+                }
+                else if (args.Length.Equals(0))
+                {
+                    Player.MessageFrom("SpawnsManager", green + "Makes sure you shoose a name");
+                }
+                else
+                {
+                    if (!Player.Equals(null))
                     {
-                        Player.MessageFrom("SpawnsManager", red + "You are not an administrator to use this command.");
-                        return;
-                    }
-                    else
-                    {
-                        if (args.Length > 0)
-                        {
-                            if (Name.Contains(args[0]))
-                            {
-                                Player.MessageFrom("SpawnsManager", "This spawn is already added.");
-                                return;
-                            }
-                            else
-                            {
-                                float x = Player.X;
-                                float y = Player.Y;
-                                float z = Player.Z;
-                                ini.AddSetting(args[0], (Player.X) + ", " + (Player.Y) + ", " + (Player.Z));
-                                Player.MessageFrom("SpawnsManager", green + "Spawn added");
-                                Spawns.Save();
-                            }
-                        }
-                        else
-                        {
-                            Player.MessageFrom("SpawnsManager", "You need to add a name of the spawn");
-                            return;
-                        }
+                        float x = Player.X;
+                        float y = Player.Y;
+                        float z = Player.Z;
+                        Settings.AddSetting(args[0], (Player.X) + ", " + (Player.Y) + ", " + (Player.Z));
+                        Settings.Save();
+                        Player.MessageFrom("SpawnsManager", green + "Spawn added");
                     }
                 }
             }
@@ -99,10 +84,11 @@ namespace SpawnsManager
         public void OnPlayerSpawned(Fougerite.Player player, SpawnEvent se)
         {
             var location = player.Location;
-            {
-                int r = rnd.Next(1, 10);
-                string l = ini.GetSetting("Spawns", r.ToString());
-                Vector3 v = Util.GetUtil().ConvertStringToVector3(l);
-                Player.TeleportTo(Spawns, false);
-            }
+            float x = player.X;
+            float y = player.Y;
+            float z = player.Z;
+            player.TeleportTo(;
+        }   //////  still under development
+    }
+}
 
